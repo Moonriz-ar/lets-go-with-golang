6d91892970bcbdc19e2003934bc09f56bce06999
Andrea Lin <andre93.lin@gmail.com>
2023-01-04 12:22:24 -0300

    struct - embedded fields

--- 11mystructs/main.go
+++ 11mystructs/main.go
@@ -9,6 +9,19 @@ func main() {
 	fmt.Println(andrea)
 	fmt.Printf("Andrea's details are: %+v\n", andrea)
 	fmt.Println(andrea.Name)
+
+	// instantiate and initialize a student
+	mark := Student{Human{"Mark", 14, 70}, "Computer Science"}
+
+	fmt.Println("His name is ", mark.name)
+	fmt.Println("His age is ", mark.age)
+	fmt.Println("His weight is ", mark.weight)
+	fmt.Println("His specialty is ", mark.specialty)
+
+	// modify mark's specialty
+	mark.specialty = "Business Administration"
+	fmt.Println("Mark changed his specialty")
+	fmt.Println("His specialty is ", mark.specialty)
 }
 
 // User of an application
@@ -17,4 +30,19 @@ type User struct {
 	Email  string
 	Status bool
 	Age    int
+}
+
+// embedded fields in struct
+
+// Human represents a person with name, age and weight
+type Human struct {
+	name   string
+	age    int
+	weight int
+}
+
+// Student represents a Human with specialty
+type Student struct {
+	Human     // embedded field, it means Student struct includes all fields that Human has
+	specialty string
 }
